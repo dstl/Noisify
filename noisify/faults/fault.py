5b925ed9b41ebe97024097ec1dcec6d001f8d6a9
declan <dcrew@dstl.gov.uk>
2019-02-12 16:23:50 +0000

    Add optional static typed dispatch to implementations.

--- noisify/faults/fault.py
+++ noisify/faults/fault.py
@@ -1,7 +1,10 @@
-from sigyn.helpers import SavedInitStatement
+from noisify.helpers import SavedInitStatement
+from pprint import pformat
+from functools import singledispatch, update_wrapper
+from typing import get_type_hints
 
 
-def register_implementation(priority=100):
+def register_implementation(priority=-1):
     def wrap(func):
         func._priority = priority
         return func
@@ -15,7 +18,6 @@ class MultipleDispatch(type):
 
     def __new__(cls, name, base, attrs):
         implementations = [(method, method._priority) for method in attrs.values() if hasattr(method, '_priority')]
-        implementations.sort(key=lambda x: x[1])
         if implementations:
             attrs['_implementations'] = [i for i in implementations]
             for parent_implementations in (getattr(b, '_implementations', None) for b in base):
@@ -23,6 +25,17 @@ class MultipleDispatch(type):
                     attrs['_implementations'] += parent_implementations
             attrs['_implementations'].sort(key=lambda x: x[1], reverse=True)
         return super(MultipleDispatch, cls).__new__(cls, name, base, attrs)
+
+
+def multiple_method_dispatch(method):
+    dispatcher = singledispatch(method)
+
+    def wrapper(*args, **kwargs):
+        return dispatcher.dispatch(args[1].__class__)(*args, **kwargs)
+    wrapper.register = dispatcher.register
+    dispatcher.registry = {}
+    update_wrapper(wrapper, method)
+    return wrapper
 
 
 class Fault(SavedInitStatement, metaclass=MultipleDispatch):
@@ -43,15 +56,32 @@ class Fault(SavedInitStatement, metaclass=MultipleDispatch):
 
     def impact(self, impacted_object):
         for implementation, priority in self._implementations:
+            type_hints = get_type_hints(implementation)
+            if type_hints:
+                accepted_type = [i for i in type_hints.values()][0]
+                if isinstance(impacted_object, accepted_type):
+                    return implementation(self, impacted_object)
+                else:
+                    continue
             try:
                 return implementation(self, impacted_object)
-            except(TypeError):
+            except TypeError:
                 continue
-            except(AttributeError):
+            except AttributeError:
                 continue
-            except(ImportError):
+            except ImportError:
                 continue
         raise NotImplementedError
+
+    @property
+    def name(self):
+        return type(self).__name__
+
+    def __repr__(self):
+        return 'Fault: %s %s' % (self.name, self.init_statement)
+
+    def formatted_string(self, indent=0):
+        return pformat(self, indent=indent)
 
 
 class AttributeFault(Fault):
@@ -59,7 +89,8 @@ class AttributeFault(Fault):
     def map_fault(self, truth_object):
         try:
             for attribute, value in truth_object.items():
-                truth_object[attribute] = self.impact(value)
+                if self.condition(value):
+                    truth_object[attribute] = self.impact(value)
             return truth_object
         except AttributeError:
             raise TypeError
